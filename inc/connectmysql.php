2a19ec8eaa9ae763e877bce0397ef0ee380324d0
josetahiry <jose.tahiry.rak@gmail.com>
2018-03-07 14:44:20 +0300

    new

--- inc/connectmysql.php
+++ inc/connectmysql.php
@@ -1,10 +1,10 @@
 <?php 
 	function getmysql(){
-		$PARAM_hote='localhost'; // le chemin vers le serveur
-		$PARAM_port='3306';
-		$PARAM_nom_bd='testpdo'; // le nom de votre base de données
-		$PARAM_utilisateur='root'; // nom d'utilisateur pour se connecter
-		$PARAM_mot_passe='root'; // mot de passe de l'utilisateur pour se connecter
+		$PARAM_hote='192.168.1.109'; // le chemin vers le serveur
+		$PARAM_port='90';
+		$PARAM_nom_bd='master'; // le nom de votre base de données
+		$PARAM_utilisateur='itu'; // nom d'utilisateur pour se connecter
+		$PARAM_mot_passe='itu'; // mot de passe de l'utilisateur pour se connecter
 		try{
 			$connexion = new PDO('mysql:host='.$PARAM_hote.';port='.$PARAM_port.';dbname='.$PARAM_nom_bd, $PARAM_utilisateur, $PARAM_mot_passe);
 			return $connexion;
