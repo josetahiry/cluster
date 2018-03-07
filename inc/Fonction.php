6d4a2649fe06d936b3f6392064e0766552263aee
josetahiry <jose.tahiry.rak@gmail.com>
2018-03-07 15:13:11 +0300

    new

--- inc/Fonction.php
+++ inc/Fonction.php
@@ -4,7 +4,7 @@
       $users='itu';
       $pass='itu';
       $port=3306;
-      $host='192.168.1.109';
+      $host='localhost';
       $dbname='master';
       $dsn='mysql:host='.$host.';port='.$port.';dbname='.$dbname;      	
 	try {
